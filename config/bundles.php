8d393e0502e4bc60fcfc5d20d158552483119f8a
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-11-19 10:32:19 +0100

    feat(security): add reset/forgot password actions and templates

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,15 +50,25 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-//                (new TemplatedEmail())
-//                    ->from(new Address('hugo.nicolas.hn@gmail.com', 'SnowTricks Admin'))
-//                    ->to($user->getEmail())
-//                    ->subject('Please Confirm your Email')
-//                    ->htmlTemplate('registration/confirmation_email.html.twig')
-//            );
-            // do anything else you need here, like send an email
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('hugo.nicolas.hn@gmail.com', 'SnowTricks Admin'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+//             do anything else you need here, like send an email
 
+            $this->addFlash(
+                'success',
+                'Your registration was successful, an email has been sent to '.$user->getEmail().' to verify your address'
+            );
+            $this->addFlash(
+                'warning',
+                'Note that you have to be logged in first to verify your email !'
+            );
             return $this->redirectToRoute('app_login');
         }
 
@@ -76,7 +86,7 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
+            $this->addFlash('error', $exception->getReason());
 
             return $this->redirectToRoute('app_register');
         }
