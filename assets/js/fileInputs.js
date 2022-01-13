c89ef8fdb939f61523dc81776f355a4649867ee9
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2022-01-13 14:44:17 +0100

    chore(CS): Improve codacy analysis note

--- assets/js/fileInputs.js
+++ assets/js/fileInputs.js
@@ -10,18 +10,18 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('#mainPictureEdit').on('change', function () {
         $('#editMainPictureForm').submit();
-    })
+    });
 });
 
 $(document).ready(function () {
     $('.click').click(function (e) {
         e.preventDefault();
        $('.click').prev('.inputFile').click();
-    })
+    });
 });
 
 $(document).ready(function() {
     $('.inputFile').on('change', function () {
         $('.inputFile').parent('.fileForm').submit();
-    })
+    });
 });
